67b88014e2555dd37d0f63c64e8499c62c774ab5
bhaskararaosanta <bhaskararaosanta@gmail.com>
2019-08-04 22:15:16 +0530

    first commit

---
+++ comma_operator.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+	int a = 0, b = 1, c = 2, d = 3;
+	//if(b, c, d, a)
+	// The above if condition will evaluate from left to right and finally execute a value and its 0 and become false
+	if(a, b, c, d)
+	printf("%d\t%d\t%d\t%d\n", a, b, c, d);
+
+	return 0;
+}
